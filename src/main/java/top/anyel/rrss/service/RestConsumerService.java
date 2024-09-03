a47767b5be48acc029579fb7d3e8a528b996cf83
anyel-ec <paulangel826@gmail.com>
2024-09-02 21:11:25 -0500

    add url to rest template

--- src/main/java/top/anyel/rrss/service/RestConsumerService.java
+++ src/main/java/top/anyel/rrss/service/RestConsumerService.java
@@ -12,16 +12,60 @@ public class RestConsumerService {
     private final RestConsumerRepository restConsumerRepository;
     private final ICustomLogger logger;
 
+
+    // Consumer the service of RestConsumerRepository
     @Autowired
     public RestConsumerService(RestConsumerRepository restConsumerRepository, CustomLoggerFactory loggerFactoryService) {
         this.logger = loggerFactoryService.getLogger(RestConsumerRepository.class);
-        this.restConsumerRepository = restConsumerRepository;
+        try {
+            this.restConsumerRepository = restConsumerRepository;
+        }
+        catch (Exception e) {
+            logger.logError("Error creating RestConsumerService: " + e.getMessage());
+            throw e;
+        }
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // get user using consumer rest
     public String getUserAsJson() {
+        try{
         logger.logInfo("Requesting all users of service");
         return restConsumerRepository.getUserAsJson();
+        }
+        catch (Exception e) {
+            logger.logError("Error getting all users: " + e.getMessage());
+            throw e;
+        }
     }
+
+
+
+
+
+
+
+
+
+
 
     public String getUserByID(Long userId) {
         logger.logInfo("Requesting user with ID: " + userId + " OF SERVICE");
